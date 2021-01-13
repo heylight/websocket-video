fa8d4d1196b5527745d3b391c1fb016f135b438a
zhuhailiang4 <zhuhailiang@jd.com>
2021-01-13 18:31:19 +0800

    1

--- socket.js
+++ socket.js
@@ -2,7 +2,7 @@ module.exports = function handleSocket(socket) {
   console.log("connected!!!");
   socket.on("fps", (res) => {
     socket.broadcast.emit("transfer", { type: "base64", data: res.data });
-    // this.emit("received");
+    this.emit("received");
   });
   socket.on("disconnect", () => {
     console.log("hello");
