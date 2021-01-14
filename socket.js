3d524167b72c9f4fa54e00c667715264f9569e8a
zhuhailiang4 <zhuhailiang@jd.com>
2021-01-14 18:33:02 +0800

    update

--- socket.js
+++ socket.js
@@ -1,20 +1,19 @@
-module.exports = function handleSocket(socket) {
-  console.log("connected!!!");
-  socket.on("create-camera", (cameraId) => {
-    socket.to(cameraId).broadcast.emit("transfer", {
-      cameraId: res.cameraId,
-      data: res.data,
+let rooms = [];
+module.exports = function handleSocket(server) {
+  const io = require("socket.io")(server);
+  io.on("connection", (socket) => {
+    let room = socket.handshake.query.cameraId || socket.id;
+    console.log("room:", room);
+    // 一个链接一个房间
+    socket.join(room);
+    socket.on("fps", (base64) => {
+      // 继续轮询
+      io.in(room).emit("received");
+      // 广播
+      socket.to(room).broadcast.emit("transfer", base64);
     });
-    this.to(cameraId).emit("received", { cameraId: res.cameraId });
-  });
-  socket.on("fps", (res) => {
-    socket.to(cameraId).broadcast.emit("transfer", {
-      cameraId: res.cameraId,
-      data: res.data,
+    socket.on("disconnect", () => {
+      console.log("disconnect");
     });
-    this.to(cameraId).emit("received", { cameraId: res.cameraId });
-  });
-  socket.on("disconnect", () => {
-    console.log("hello");
   });
 };
